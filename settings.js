28c72a1d0242e11047b54f1cdcbfb5d652c31be2
swtichedxp <vinixspace@outlook.com>
2025-08-07 05:28:05 -0400

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '𝑨𝒏𝒅𝒓𝒐𝒎𝒆𝒅𝒂 𝕏Ɽ',
+  author: '',
+  botName: "𝑨𝒏𝒅𝒓𝒐𝒎𝒆𝒅𝒂 𝕏Ɽ",
+  botOwner: 'zed🛸', // Your name
+  ownerNumber: '212684255286', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "a next-generation intelligent agent designed to seamlessly integrate with and enhance extended tasks and commands.",
+  version: "1.0",
+};
+
+module.exports = settings;
