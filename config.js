4712c0e21ea9175500bc2d6c6694144f6f3aa1ed
Giftfx-ship <nwodugift5@gmail.com>
2025-08-11 22:02:25 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,61 @@
+module.exports = {
+    botName: "DEVMD",
+    ownerName: "𝐌𝐑ܮ𝐃𝐄𝐕『ᴾᴿᴵ́ᴹᴱ́』",
+    ownerNumber: "2349164624021",
+    ownerContactLink: `https://wa.me/2349164624021`,
+    github: "https://github.com/Giftfx-ship/Devmd",
+    channel: "https://whatsapp.com/channel/0029VbB3zXu9Gv7LXS62GA1F",
+    
+    prefix: ".",
+    
+    aliveMessage: `Hello! I am *DEVMD*, powered by 𝐌𝐑ܮ𝐃𝐄𝐕『ᴾᴿᴵ́ᴹᴱ́』`,
+    
+    commands: {
+        owner: [
+            "autoviewstatus",
+            "restart",
+            "shutdown",
+            "broadcast",
+            "block",
+            "unblock",
+            "mode",
+            "clearsession",
+            "setpp"
+        ],
+        group: [
+            "antilink",
+            "kick",
+            "tagall",
+            "mute",
+            "unmute",
+            "promote",
+            "demote",
+            "welcome",
+            "goodbye"
+        ],
+        general: [
+            "alive",
+            "ping",
+            "sticker",
+            "toimg",
+            "tts",
+            "weather",
+            "github",
+            "meme",
+            "fact",
+            "joke",
+            "quote",
+            "play",
+            "song",
+            "simage",
+            "tag",
+            "warn",
+            "warnings"
+        ],
+        ai: [
+            "ai <query>",
+            "imagine <prompt>",
+            "chatbot on/off"
+        ]
+    }
+}
