8a0a03735326dc60af3aeec8187bf4e4b93e77fd
Giftfx-ship <nwodugift5@gmail.com>
2025-08-12 21:23:48 +0100

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,17 +1,19 @@
 // unmute.js
-async function unmuteCommand(sock, chatId) {
+async function unmuteCommand(sock, chatId, message = null) {
     try {
         // Change group setting to allow members to send messages
         await sock.groupSettingUpdate(chatId, 'not_announcement');
 
         // Send confirmation message to the group
         await sock.sendMessage(chatId, {
-            text: '✅ The group has been unmuted by Mr Dev ❤️ Let’s talk, guys!'
-        });
+            text: '✅ The group has been unmuted by Mr Devmd ❤️ Let’s talk, guys!'
+        }, message ? { quoted: message } : {});
 
     } catch (error) {
         console.error('Error unmuting group:', error);
-        await sock.sendMessage(chatId, { text: '❌ Failed to unmute the group.' });
+        await sock.sendMessage(chatId, {
+            text: '❌ Failed to unmute the group.'
+        }, message ? { quoted: message } : {});
     }
 }
 
