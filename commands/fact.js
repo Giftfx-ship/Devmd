36f8c6400e0c95fce6d04db510b1c2a1c3e942a8
Giftfx-ship <nwodugift5@gmail.com>
2025-08-12 18:45:44 +0100

    Update fact.js

--- commands/fact.js
+++ commands/fact.js
@@ -1,12 +1,29 @@
 const axios = require('axios');
 
+const settings = {
+  botName: 'MR DEV',
+  channel: 'https://youtube.com/@mrdev'
+};
+
+const channelInfo = {
+  footer: `Created by ${settings.botName} | Join channel: ${settings.channel}`
+};
+
 module.exports = async function (sock, chatId, message) {
-    try {
-        const response = await axios.get('https://uselessfacts.jsph.pl/random.json?language=en');
-        const fact = response.data.text;
-        await sock.sendMessage(chatId, { text: fact },{ quoted: message });
-    } catch (error) {
-        console.error('Error fetching fact:', error);
-        await sock.sendMessage(chatId, { text: 'Sorry, there has been some communication interference and I could not  get that fact to come through.' },{ quoted: message });
-    }
+  try {
+    const response = await axios.get('https://uselessfacts.jsph.pl/random.json?language=en');
+    const fact = response.data.text;
+
+    await sock.sendMessage(chatId, { 
+      text: fact,
+      ...channelInfo
+    }, { quoted: message });
+
+  } catch (error) {
+    console.error('Error fetching fact:', error);
+    await sock.sendMessage(chatId, { 
+      text: 'Sorry, there has been some communication interference and I could not get that fact to come through.',
+      ...channelInfo
+    }, { quoted: message });
+  }
 };
