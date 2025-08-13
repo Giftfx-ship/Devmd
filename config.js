5c69f06e80a1596d12250bdceb0125ca4a75d16e
Giftfx-ship <nwodugift5@gmail.com>
2025-08-13 15:38:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,18 +2,19 @@ module.exports = {
   botName: "DEVMD",
   ownerName: "𝐌𝐑ܮ𝐃𝐄𝐕",
   ownerNumber: "2349164624021",
-  ownerContactLink: `https://wa.me/2349164624021`,
+  ownerContactLink: "https://wa.me/2349164624021",
   github: "https://github.com/Giftfx-ship/Devmd",
   channel: "https://whatsapp.com/channel/0029VbB3zXu9Gv7LXS62GA1F",
   
   prefix: ".",
-  
+
   aliveMessage: `Hello! I am *DEVMD*, powered by 𝐌𝐑ܮ𝐃𝐄𝐕『ᴾᴿᴵ́ᴹᴱ́』`,
 
   sessionFolder: "./session",
-  
+
   updateCheckIntervalMs: 10 * 60 * 1000, // 10 minutes
-  
+
+  // 🔰 Commands grouped by category
   commands: {
     owner: [
       "autoviewstatus"
@@ -22,10 +23,10 @@ module.exports = {
       "antilink delete",
       "antilink kick",
       "kick",
-      "tag",
+      "tag",       // silent tagall
       "mute",
-      "unmute",
-      "hackjc",
+      "unmute",    // open group
+      "hackgc",
       "promote",
       "demote"
     ],
